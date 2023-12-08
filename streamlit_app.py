020ac7e0668069e1181fc07cd6e67ca18bf9fa15
Kavita K <kavita@curvetomorrow.com.au>
2023-12-08 13:34:23 +0530

    Another version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import openai
-from langchain.chains import RetrievalQA
+# from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import CSVLoader
 from langchain.vectorstores import DocArrayInMemorySearch
